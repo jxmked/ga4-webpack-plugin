2d21d8139d55a9359d0114603ce6a25f6d6295e7
jxmked <jovandeguia@gmail.com>
2023-02-04 17:58:05 +0800

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # ga4-webpack-plugin
-Injecting Google snippet into HTML for Analytics
+
+Injecting gtag snippet into HTML for Analytics
+
+## Install
+
+`npm install ga4-webpack-plugin --save-dev`
+
+## Setup
+
+```js
+// webpack.config.js
+
+const GA4WebpackPlugin = require('ga4-webpack-plugin');
+
+module.exports = {
+  ....
+  
+  plugins: [
+    new GA4WebpackPlugin({
+      // Your GTag ID. Required
+      id: "GA_MEASUREMENT_ID",
+      
+      // Insert into html (true by default)
+      inject: true | false,
+      
+      // Automatically call page view (true by default)
+      callPageView: true | false 
+    })
+  ]
+}
+```
+
+Place this `<ga4.analytics />` in to your index.html where to inject the gtag or else it won't inject it.
+
+## What kind of thing is about to inject?
+
+```html
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'GA_MEASUREMENT_ID');
+</script>
+```
+
+but 
+
+```js
+gtag('js', new Date());
+gtag('config', GA_MEASUREMENT_ID');
+```
+
+is optional. Set `callPageView: true` to insert it.
---
+++ index.js
@@ -0,0 +1,78 @@
+class Plugin {
+  static src = 'https://www.googletagmanager.com/gtag/js?id=%ID%';
+  static html = `
+  <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+  </script>`;
+  static pageView = `gtag('js', new Date());gtag('config', '%ID%');`;
+
+  constructor(options) {
+    if (options.id === void 0) {
+      throw new Error('GA4WebpackPlugin requires GA4 id');
+    }
+
+    this.callPageView = true;
+    this.inject = true;
+
+    // GA4 ID
+    this.id = options.id;
+
+    /**
+     * Inject only on build mode
+     * */
+    if ('inject' in options) this.inject = options.inject;
+
+    /**
+     * Call Page View
+     * */
+    if ('callPageView' in options) this.callPageView = options.callPageView;
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('ModifyIndexHtmlPlugin', (compilation, callback) => {
+      if (!this.inject) {
+        callback();
+        return;
+      }
+
+      const indexHtml = compilation.assets['index.html'];
+      const keyid = '%ID%';
+
+      /**
+       * Interpolate template
+       * */
+      const src = Plugin.src.replaceAll(keyid, this.id);
+      const pageView = Plugin.pageView.replaceAll(keyid, this.id);
+
+      let gtag = '\n' + '<script async src="' + src + '"></script>';
+
+      if (this.callPageView) {
+        gtag += Plugin.html.replace('</script>', pageView + '</script>');
+      } else {
+        gtag += Plugin.html;
+      }
+
+      // Remove new lines and extra spaces
+      gtag = gtag.replaceAll(/(\n+|\s{2,})*/gi, '');
+
+      // <ga4.analytics />
+      // <ga4.analytics/>
+      const reg = /\<ga4\.analytics\ ?\/\>/i;
+
+      // Injecting...
+
+      const modifiedContent = indexHtml.source().replace(reg, gtag);
+
+      // Update build index.html
+      compilation.assets['index.html'] = {
+        source: () => modifiedContent,
+        size: () => modifiedContent.length
+      };
+
+      callback();
+    });
+  }
+}
+
+module.exports = Plugin;
