b8683469cc44f17f4cf49d7c49e25c1b115c16a6
jxmked <jovandeguia@gmail.com>
2026-03-22 08:24:31 +0800

    Fixes: String

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ class Plugin {
     let pageView = `gtag('js', new Date());gtag('config', '${this.id}'`;
 
     if (typeof this.appVersion === "string") {
-      pageView += `,{"app_version":${this.appVersion}}`
+      pageView += `,{"app_version":"${String(this.appVersion)}"}`;
     }
 
     pageView += `);`;
